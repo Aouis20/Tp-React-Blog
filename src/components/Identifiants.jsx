e2104ecf8547eed183fc614850456dd114cb609c
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2022-06-03 11:30:18 +0200

    Update Identifiants.jsx

--- src/components/Identifiants.jsx
+++ src/components/Identifiants.jsx
@@ -23,7 +23,7 @@ const Identifiants = ({username, mdp, type}) => {
                     })
                 ) : (
                     console.log('oui')
-                )z
+                )
             }
         </div>
     )
